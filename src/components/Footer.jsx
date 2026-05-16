4e8e2579fccfff67111241402e4d910d767cd4ac
naderzayan <naderahmed.na49@gmail.com>
2026-05-16 22:01:53 +0300

    logo

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -8,7 +8,7 @@ export default function Footer() {
               Infinity Technology
           </a>
           <a href="https://infntytech.com/" target="_blank" rel="noopener">
-              <img src="https://infntytech.com/wp-content/uploads/2024/09/Infinity-removebg-preview.png" className="image" />
+              <img src="logo 2026 Transparent.png" className="image" />
           </a>
           <span> Powered By </span>
       </main>
